f0913b5d520b9e41417413de52d301b57bc62da2
1gyou1 <1gyou1@naver.com>
2024-06-13 21:50:28 +0900

    gallery api postman 관련 수정

--- dinotaeng/src/app/api/gallery/list/route.ts
+++ dinotaeng/src/app/api/gallery/list/route.ts
@@ -10,7 +10,7 @@ export async function GET (request: Request) {
         return Response.json({
             'result'    : true,
             'message'   : '',
-            'data'      : []
+            'data'      : galleryData
         })
     } catch (e:any) {
         return Response.json({
