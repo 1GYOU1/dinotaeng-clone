853496e6031156fceefb7f44f8d817b563f0c659
1gyou1 <1gyou1@naver.com>
2024-06-13 22:00:00 +0900

    gallery page 생성

---
+++ dinotaeng/src/app/gallery/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+function Page() {
+  return (
+    <div className="w-full">
+    <p>gallery</p>
+    <Link href={`https://dinotaeng.com/mundane/html/product/journal_detail.html?product_no=249&cate_no=1&display_group=12`} target="_blank">https://dinotaeng.com/mundane/html/product/journal_detail.html?product_no=249&cate_no=1&display_group=12</Link>
+    </div>
+  )
+}
+
+export default Page
