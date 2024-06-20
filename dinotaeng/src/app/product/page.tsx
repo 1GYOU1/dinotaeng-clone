8772d88f12319b2f00d1702bd1c41308914246ec
hyennii <alqkf@icloud.com>
2024-06-20 16:42:12 +0900

    상품 api 연결 시도1

--- dinotaeng/src/app/product/page.tsx
+++ dinotaeng/src/app/product/page.tsx
@@ -1,20 +1,42 @@
-import Link from "next/link"
+'use client'
 
-import Header from "../header"
-import Footer from "../footer"
-import styled from "../subpage.module.scss"
+import Link from "next/link";
+import Header from "../header";
+import Footer from "../footer";
+import styled from "../subpage.module.scss";
 
+import { useEffect, useState } from "react";
+import { fetchShopList } from "@/fetch/fetchShop";
+
+interface Shop {
+  productId: number;
+  productName: string;
+  productThumbnail: string;
+  productPrice: number;
+  productSalePrice?: number;
+}
 
 function Page() {
+  const [shopData, setShopData] = useState<Shop[] | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const data = await fetchShopList({});
+      if (data.result && data.data) {
+        setShopData(data.data);
+      } else {
+        console.error(data.message);
+      }
+    };
+    init();
+  }, []);
+
   return (
     <div className="w-full">
-
-      <Header/>
-      {/* <Link href={`https://dinotaeng.com/product/list.html?cate_no=42`} target="_blank">https://dinotaeng.com/product/list.html?cate_no=42</Link> */}
+      <Header />
 
       <div className={styled.product_area}>
         <div className={styled.cont_wrap}>
-
           <ul className={styled.category}>
             <li><Link href="">NEW</Link></li>
             <li><Link href="">BEST</Link></li>
@@ -27,45 +49,30 @@ function Page() {
 
           <div className={styled.flex_wrap}>
             <ul>
-              <li>
-                <Link href="">
-                  <div className={styled.img_box}>
-                    <img className={styled.image} src="/images/shop_img_1.png" alt="상품 이미지1" />
-                    <img className={styled.image_hover} src="/images/shop_hover_img_1.png" alt="상품 이미지1" />
-                  </div>
-                  <p>SHHH! Don't move BOBO Mini Figure</p>
-                </Link>
-                <span>₩16,000</span>
-              </li>
-              <li>
-                <Link href="">
-                  <div className={styled.img_box}>
-                    <img className={styled.image} src="/images/shop_img_2.png" alt="상품 이미지2" />
-                    <img className={styled.image_hover} src="/images/shop_hover_img_2.png" alt="상품 이미지2" />
-                  </div>
-                  <p>BOBO in the Woods MINI LAMP</p>
-                </Link>
-                <span>₩29,000</span>
-              </li>
-              <li>
-                <Link href="">
-                  <div className={styled.img_box}>
-                    <img className={styled.image} src="/images/shop_img_3.png" alt="상품 이미지3" />
-                    <img className={styled.image_hover} src="/images/shop_hover_img_3.png" alt="상품 이미지3" />
-                  </div>
-                  <p>APPLE PICKING WATCH CHARGER STAND</p>
-                </Link>
-                <span>₩29,000</span>
-              </li>
+              {shopData ? (
+                shopData.map((item) => (
+                  <li key={item.productId}>
+                    <Link href={`/product/${item.productId}`}>
+                      <div className={styled.img_box}>
+                        <img className={styled.image} src={item.productThumbnail} alt={`상품 이미지 ${item.productName}`} />
+                        <img className={styled.image_hover} src={item.productThumbnail} alt={`상품 이미지 ${item.productName}`} />
+                      </div>
+                      <p>{item.productName}</p>
+                    </Link>
+                    <span>₩{item.productPrice.toLocaleString()}</span>
+                  </li>
+                ))
+              ) : (
+                <p>상품을 불러오는 중입니다...</p>
+              )}
             </ul>
           </div>
-
         </div>
       </div>
 
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
