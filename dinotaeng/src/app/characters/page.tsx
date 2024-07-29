3fd854f75a93c600d3375259aa035b4cc31e7820
1gyou1 <1gyou1@naver.com>
2024-07-29 23:28:41 +0900

    캐릭터 페이지 상세 팝업 API 연동, slideTo, 팝업 열고 닫기 기능

--- dinotaeng/src/app/characters/page.tsx
+++ dinotaeng/src/app/characters/page.tsx
@@ -9,6 +9,7 @@ import { fetchCharactersList } from "@/fetch/fetchCharacters"
 // swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, EffectFade } from 'swiper/modules';
+import SwiperCore from 'swiper'; // Swiper 핵심 기능 제어 클래스
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
@@ -21,6 +22,8 @@ interface Props {
 function Page() {
 
   const [charactersData, setCharactersData] = useState<Characters[]>()
+  const [characterPopShow, setCharacterPopShow] = useState(false);
+  const [swiper, setSwiper] = useState<SwiperCore>(); // sliderTo
 
   useEffect(() => {
 
@@ -32,6 +35,20 @@ function Page() {
     init()
 
   }, [])
+
+  // 슬라이드 팝업 열고 닫기
+  const popShowEvt = (e?: number) => {
+    if (characterPopShow) {
+      setCharacterPopShow(false); // 팝업 닫기
+      document.body.style.overflow = 'auto'; // 스크롤 복원
+    } else {
+      setCharacterPopShow(true); // 팝업 열기
+      if (swiper && e) {
+        swiper.slideTo(e); // 슬라이드 idx 이동
+      }
+      document.body.style.overflow = 'hidden'; // 스크롤 방지
+    }
+  };
 
   return (
     <>
@@ -46,9 +63,9 @@ function Page() {
           <h4>Meet our villagers</h4>
           <div className={styled.img_box}>
             <ul>
-            {charactersData && charactersData.map((item: Characters) => (
+            {charactersData && charactersData.map((item: Characters, idx: number) => (
               <li key={item._id}>
-                <div className={styled.s_btn}>
+                <div className={styled.s_btn} onClick={() => popShowEvt(idx)}>
                   <img src={item.thumbnail} alt={item.name}/>
                 </div>
               </li>
@@ -58,7 +75,7 @@ function Page() {
         </div>
         
         {/* 팝업 */}
-        <div className={styled.characters_pop_layer}>
+        <div className={`${styled.characters_pop_layer} ${characterPopShow ? styled.on : ''}`}>
             <>
 							<Swiper className={`${styled.event_slide} event_slide fade`}
                 modules={[Navigation, EffectFade]}
@@ -66,16 +83,18 @@ function Page() {
 								speed={500}
                 effect='fade'
 								slidesPerView={1}
+                onSwiper={setSwiper} // state 업뎃
+                observer={true} // 부모 요소 변화 감지
 								navigation={{ prevEl: ".swiper-arrow-prev", nextEl: ".swiper-arrow-next" }}>
                   {charactersData && charactersData.map((item: Characters) => (
-                    <SwiperSlide>
-                    <li>
-                      <div className={styled.thumb}>
-												<img src={item.thumbnail} alt={item.name}/>
-                      </div>
-											<div className={styled.name}><span>{item.name}</span></div>
-                      <div className={styled.desc}>{item.description}</div>
-										</li>
+                    <SwiperSlide key={item._id}>
+                      <li>
+                        <div className={styled.thumb}>
+                          <img src={item.thumbnail} alt={item.name}/>
+                        </div>
+                        <div className={styled.name}><span>{item.name}</span></div>
+                        <div className={styled.desc}>{item.description}</div>
+                      </li>
                     </SwiperSlide>
                   ))}
 								</Swiper>
@@ -83,7 +102,7 @@ function Page() {
 								<div className={`${styled.slideArrowNext} swiper-arrow-next`}></div>
 							</>
         </div>
-        <div className={styled.characters_pop_bg}></div>
+        <div className={`${styled.characters_pop_bg} ${characterPopShow ? styled.on : ''}`} onClick={() => popShowEvt()}></div>
 
       </div>
 
